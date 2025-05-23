68c9119c5fa3a2e6deae074002882662cc551e67
# <me@rebound.sh>
2025-05-23 15:49:42 -0700

    feat: constructor validation

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -3,6 +3,24 @@ use std::cmp::Ordering;
 
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("bad value");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     let num = rand::thread_rng().gen_range(1..=10);
 

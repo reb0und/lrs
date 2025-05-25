d5922dd348da75ff36ef8de0d8851e9f3c922186
# <me@rebound.sh>
2025-05-25 01:28:10 -0700

    feat: traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,22 @@
-//use std::{cmp::Ordering, io};
-use std::io::{self, Write};
+pub struct NewsArticle {
+    pub headline: String,
+    pub author: String,
+    pub content: String,
+}
 
-mod front_of_house;
-
-fn deliver_order() {}
-
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{} {} {}", self.headline, self.author, self.content)
     }
 
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    fn fix_bad_order() {
-        super::deliver_order()
+    fn summarize_author(&self) -> String {
+        format!("{}", self.author)
     }
 }
 
-pub use crate::front_of_house::hosting;
-
-pub fn eat_at_rest() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-
-    meal.toast = String::from("Wheat");
-    //meal.seasonal_fruit = String::from("ax");
-    crate::front_of_house::hosting::add_to_waitlist();
-    hosting::add_to_waitlist();
-
-    let order1 = back_of_house::Appetizer::Soup;
-
-    front_of_house::hosting::add_to_waitlist();
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("{}", self.summarize_author())
+    }
 }

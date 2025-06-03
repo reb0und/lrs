d918912cce39cf01ed4475b84b02ee041fa02770
# <me@rebound.sh>
2025-06-02 22:37:40 -0700

    test: example tests

---
+++ src/tests.rs
@@ -0,0 +1,105 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+pub fn add_two(i: i32) -> i32 {
+    i + 2
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value > 100 || value < 100 {
+            panic!("input out of bounds");
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.height && self.height > other.height
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn exploration() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+    
+    //#[test]
+    //fn another() {
+    //    panic!("fail");
+    //}
+
+    #[test]
+    #[ignore]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 7,
+            height: 6,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+
+        let smaller = Rectangle {
+            width: 7,
+            height: 6,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[should_panic(expected = "out of bounds")]
+    fn greater_than_100() {
+        Guess::new(101);
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("2 + 2 != 4"))
+        }
+    }
+}

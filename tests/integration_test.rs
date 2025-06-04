5f0aebf51cfcb86776c63ac5b57514c8e0fdf27c
# <me@rebound.sh>
2025-06-04 00:44:20 -0700

    tests: disable integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,9 @@
-use lrs::add_two;
-
-mod common;
-
-#[test]
-fn it_adds_two() {
-    let result = add_two(5);
-    assert_eq!(result, 7);
-}
+//use lrs::add_two;
+//
+//mod common;
+//
+//#[test]
+//fn it_adds_two() {
+//    let result = add_two(5);
+//    assert_eq!(result, 7);
+//}

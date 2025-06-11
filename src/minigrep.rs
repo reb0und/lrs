21718fa29ac510d1642f264060719e5f38f5260a
# <me@rebound.sh>
2025-06-10 23:07:29 -0700

    feat: minigrep

--- src/minigrep.rs
+++ src/minigrep.rs
@@ -1,6 +1,22 @@
 use std::env;
+use std::process;
+
+use lrs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file: {}", config.file_path);
+
+
+    if let Err(e) = lrs::run(config) {
+        eprintln!("Application err {e}");
+        process::exit(1);
+    }
 }

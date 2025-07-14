bfe757a4c756b30f539386429fb8a209b1ca774c
# <me@rebound.sh>
2025-07-13 22:19:01 -0700

    feat: gui oop

---
+++ src/gui_oop.rs
@@ -0,0 +1,58 @@
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("yes"),
+                    String::from("maybe"),
+                    String::from("no"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height:10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {}
+}
+
+struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {}
+}
